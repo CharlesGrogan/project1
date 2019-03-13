21f20fc2d3a8d8e80e8714640f6c9c0c84f14c93
mkdoh3 <mkdoherty3@GMAIL.COM>
2019-03-13 11:34:47 -0500

    stuff or whatever <--- a bad commit message

---
+++ app.js
@@ -0,0 +1,19 @@
+var queryURL =
+  "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=trololol";
+
+$(".btns").on("click", function() {
+  fetchLols().then(gif => {
+    const url = gif.data.images.original.url;
+    displayLols(url);
+  });
+});
+
+function fetchLols() {
+  return $.get(queryURL);
+}
+
+function displayLols(url) {
+  const img = $("<img>");
+  img.attr("src", url);
+  $("#lolol").append(img);
+}
